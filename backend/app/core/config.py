e2825dfe4fb11773f0b1e4b6d3d7e4661e78b5c4
Nicolás Huenchuquen <nicolas.huenchuquen@example.com>
2026-03-16 03:44:48 -0300

    fix: agregar variables mock de pydantic en CI

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -4,6 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     DATABASE_URL: str
     SECRET_KEY: str
+    FRONTEND_URL: str = "http://localhost:3000"
 
     model_config = SettingsConfigDict(
         env_file=".env", # Carga las variables del archivo .env que este en /backend
